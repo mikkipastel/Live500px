b97f69331f4f1b11683441bb6c7b0fca9be6dfc2
Monthira Chayabanjonglerd <monthira@fungjai.com>
2017-10-13 21:00:50 +0700

    Lecture 154 - Add an Animation to ListView's Item

--- app/src/main/java/com/mikkipastel/live500px/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/mikkipastel/live500px/adapter/PhotoListAdapter.java
@@ -2,8 +2,11 @@ package com.mikkipastel.live500px.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 
+import com.mikkipastel.live500px.R;
 import com.mikkipastel.live500px.dao.PhotoItemCollectionDao;
 import com.mikkipastel.live500px.dao.PhotoItemDao;
 import com.mikkipastel.live500px.manager.PhotoListManager;
@@ -12,6 +15,8 @@ import com.mikkipastel.live500px.view.PhotoListItem;
 public class PhotoListAdapter extends BaseAdapter {
 
     PhotoItemCollectionDao dao;
+
+    int lastPosition = -1;
 
     public void setDao(PhotoItemCollectionDao dao) {
         this.dao = dao;
@@ -48,6 +53,13 @@ public class PhotoListAdapter extends BaseAdapter {
         item.setImageUrl(dao.getImageUrl());
         item.setTextName(dao.getCaption());
         item.setTextDescription(dao.getUsername() + "\n" + dao.getCamera());
+
+        if (position > lastPosition) {
+            Animation anim = AnimationUtils.loadAnimation(parent.getContext(),
+                    R.anim.up_from_bottom);
+            item.startAnimation(anim);
+            lastPosition = position;
+        }
 
         return item;
     }
