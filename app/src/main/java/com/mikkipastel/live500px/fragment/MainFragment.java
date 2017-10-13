4eda2b7301555efa03c5ae1200b936e35225602b
Monthira Chayabanjonglerd <monthira@fungjai.com>
2017-10-13 21:14:59 +0700

    Lecture 156 - Implementing Pull to Refresh

--- app/src/main/java/com/mikkipastel/live500px/fragment/MainFragment.java
+++ app/src/main/java/com/mikkipastel/live500px/fragment/MainFragment.java
@@ -2,9 +2,11 @@ package com.mikkipastel.live500px.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -25,6 +27,8 @@ public class MainFragment extends Fragment {
 
     ListView listView;
     PhotoListAdapter listAdapter;
+
+    SwipeRefreshLayout swipeRefreshLayout;
 
     public MainFragment() {
         super();
@@ -51,10 +55,35 @@ public class MainFragment extends Fragment {
         listAdapter = new PhotoListAdapter();
         listView.setAdapter(listAdapter);
 
+        swipeRefreshLayout = rootView.findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                reloadData();
+            }
+        });
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                swipeRefreshLayout.setEnabled(firstVisibleItem == 0);
+            }
+        });
+
+        reloadData();
+    }
+
+    private void reloadData() {
         Call<PhotoItemCollectionDao> call = HttpManager.getInstance().getService().loadPhotoList();
         call.enqueue(new Callback<PhotoItemCollectionDao>() {
             @Override
-            public void onResponse(Call<PhotoItemCollectionDao> call, Response<PhotoItemCollectionDao> response) {
+            public void onResponse(Call<PhotoItemCollectionDao> call,
+                                   Response<PhotoItemCollectionDao> response) {
+                swipeRefreshLayout.setRefreshing(false);
                 if (response.isSuccessful()) {
                     PhotoItemCollectionDao dao = response.body();
                     listAdapter.setDao(dao);
@@ -77,6 +106,7 @@ public class MainFragment extends Fragment {
 
             @Override
             public void onFailure(Call<PhotoItemCollectionDao> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
                 Toast.makeText(Contextor.getInstance().getContext(),
                         t.toString(),
                         Toast.LENGTH_SHORT)
