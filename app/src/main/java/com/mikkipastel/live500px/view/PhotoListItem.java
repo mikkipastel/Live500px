0ab7455c1348db3476428ee4c57b00e698b5ae9e
Monthira Chayabanjonglerd <monthira@fungjai.com>
2017-10-13 16:20:14 +0700

    Lecture134 : Create a CustomViewGroup for Photo List Item #3

---
+++ app/src/main/java/com/mikkipastel/live500px/view/PhotoListItem.java
@@ -0,0 +1,97 @@
+package com.mikkipastel.live500px.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.AttributeSet;
+
+import com.inthecheesefactory.thecheeselibrary.view.BaseCustomViewGroup;
+import com.inthecheesefactory.thecheeselibrary.view.state.BundleSavedState;
+import com.mikkipastel.live500px.R;
+
+public class PhotoListItem extends BaseCustomViewGroup {
+
+    public PhotoListItem(Context context) {
+        super(context);
+        initInflate();
+        initInstances();
+    }
+
+    public PhotoListItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initInflate();
+        initInstances();
+        initWithAttrs(attrs, 0, 0);
+    }
+
+    public PhotoListItem(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initInflate();
+        initInstances();
+        initWithAttrs(attrs, defStyleAttr, 0);
+    }
+
+    @TargetApi(21)
+    public PhotoListItem(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initInflate();
+        initInstances();
+        initWithAttrs(attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initInflate() {
+        inflate(getContext(), R.layout.list_item_photo, this);
+    }
+
+    private void initInstances() {
+        // findViewById here
+    }
+
+    private void initWithAttrs(AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        /*
+        TypedArray a = getContext().getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.StyleableName,
+                defStyleAttr, defStyleRes);
+
+        try {
+
+        } finally {
+            a.recycle();
+        }
+        */
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+
+        BundleSavedState savedState = new BundleSavedState(superState);
+        // Save Instance State(s) here to the 'savedState.getBundle()'
+        // for example,
+        // savedState.getBundle().putString("key", value);
+
+        return savedState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        BundleSavedState ss = (BundleSavedState) state;
+        super.onRestoreInstanceState(ss.getSuperState());
+
+        Bundle bundle = ss.getBundle();
+        // Restore State from bundle here
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = width * 2 /3;
+        int newHeightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                height,
+                MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, newHeightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
+}
