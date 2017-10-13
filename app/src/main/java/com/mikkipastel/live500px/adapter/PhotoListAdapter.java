655209b2078e7a21e3ba8f57bca33b85198a2a42
Monthira Chayabanjonglerd <monthira@fungjai.com>
2017-10-13 18:10:04 +0700

    Lecture137 : Deal with Multiple View Types inside ListView

--- app/src/main/java/com/mikkipastel/live500px/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/mikkipastel/live500px/adapter/PhotoListAdapter.java
@@ -3,6 +3,7 @@ package com.mikkipastel.live500px.adapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.mikkipastel.live500px.view.PhotoListItem;
 
@@ -23,12 +24,33 @@ public class PhotoListAdapter extends BaseAdapter {
     }
 
     @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position % 2 == 0 ? 0 : 1;
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        PhotoListItem item;
-        if (convertView != null)
-            item = (PhotoListItem) convertView;
-        else
-            item = new PhotoListItem(parent.getContext());
-        return item;
+        if (getItemViewType(position) == 0) {
+            PhotoListItem item;
+            if (convertView != null)
+                item = (PhotoListItem) convertView;
+            else
+                item = new PhotoListItem(parent.getContext());
+            return item;
+        } else {
+            TextView item;
+            if (convertView != null)
+                item = (TextView) convertView;
+            else
+                item = new TextView(parent.getContext());
+            item.setText("Position: " + position);
+            return item;
+        }
+
     }
 }
