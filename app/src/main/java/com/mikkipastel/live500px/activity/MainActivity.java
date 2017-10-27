1894a0e50a2ac27d129422f36279d2b44001e693
Monthira Chayabanjonglerd <monthira@fungjai.com>
2017-10-27 16:30:58 +0700

    Lecture 164 - Updating Android Support Library to the latest version

--- app/src/main/java/com/mikkipastel/live500px/activity/MainActivity.java
+++ app/src/main/java/com/mikkipastel/live500px/activity/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 R.string.open_drawer,
                 R.string.close_drawer
         );
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
