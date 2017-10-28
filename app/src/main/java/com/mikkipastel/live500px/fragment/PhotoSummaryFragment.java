b408cccf24401e045ec0493c54b8a690d4381158
Monthira Chayabanjonglerd <monthira@fungjai.com>
2017-10-28 21:21:04 +0700

    Lecture 180 - ViewPager #3: Fragment Paging Part 2

--- app/src/main/java/com/mikkipastel/live500px/fragment/PhotoSummaryFragment.java
+++ app/src/main/java/com/mikkipastel/live500px/fragment/PhotoSummaryFragment.java
@@ -3,26 +3,20 @@ package com.mikkipastel.live500px.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.mikkipastel.live500px.R;
 
-public class MoreInfoFragment extends Fragment {
+public class PhotoSummaryFragment extends Fragment {
 
-    ViewPager viewPager;
-
-    public MoreInfoFragment() {
+    public PhotoSummaryFragment() {
         super();
     }
 
-    public static MoreInfoFragment newInstance() {
-        MoreInfoFragment fragment = new MoreInfoFragment();
+    public static PhotoSummaryFragment newInstance() {
+        PhotoSummaryFragment fragment = new PhotoSummaryFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -40,7 +34,7 @@ public class MoreInfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_more_info, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_photo_summary, container, false);
         initInstances(rootView, savedInstanceState);
         return rootView;
     }
@@ -55,27 +49,6 @@ public class MoreInfoFragment extends Fragment {
         // Init 'View' instance(s) with rootView.findViewById here
         // Note: State of variable initialized here could not be saved
         //       in onSavedInstanceState
-        viewPager = rootView.findViewById(R.id.viewPager);
-        viewPager.setAdapter(new FragmentStatePagerAdapter(getChildFragmentManager()) {
-            @Override
-            public int getCount() {
-                return 3;
-            }
-
-            @Override
-            public Fragment getItem(int position) {
-                switch (position) {
-                    case 0:
-                        return PhotoSummaryFragment.newInstance();
-                    case 1:
-                        return PhotoInfoFragment.newInstance();
-                    case 2:
-                        return PhotoTagFragment.newInstance();
-                    default:
-                        return null;
-                }
-            }
-        });
     }
 
     @Override
